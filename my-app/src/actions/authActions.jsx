c41e4a0aa82db3108ec17472cdc5e7afb81cdafc
mirzahassanI <165846334+mirzahassanI@users.noreply.github.com>
2024-04-09 20:09:30 +0500

    Update authActions.jsx

--- my-app/src/actions/authActions.jsx
+++ my-app/src/actions/authActions.jsx
@@ -1,55 +1,55 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Action types
-export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const FORGOT_PASSWORD_SUCCESS = 'FORGOT_PASSWORD_SUCCESS';
-export const AUTH_ERROR = 'AUTH_ERROR';
+export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const FORGOT_PASSWORD_SUCCESS = "FORGOT_PASSWORD_SUCCESS";
+export const AUTH_ERROR = "AUTH_ERROR";
+const baseURL = "http://localhost:5000/api";
 
 // Action creators
 export const signUp = (userData) => async (dispatch) => {
-    try {
-        const res = await axios.post('/api/signup', userData);
-        dispatch({
-            type: SIGNUP_SUCCESS,
-            payload: res.data // Assuming the backend returns user data and JWT token upon successful signup
-        });
-    } catch (error) {
-        dispatch({
-            type: AUTH_ERROR,
-            payload: error.response.data.message // Assuming the backend returns error message in case of failure
-        });
-    }
+  try {
+    const res = await axios.post(`${baseURL}/users/signup`, userData);
+    console.log("🚀 ~ signUp ~ res:", res)
+    dispatch({
+      type: SIGNUP_SUCCESS,
+      payload: res.data, // Assuming the backend returns user data and JWT token upon successful signup
+    });
+  } catch (error) {
+    dispatch({
+      type: AUTH_ERROR,
+      payload: error.response.data.message, // Assuming the backend returns error message in case of failure
+    });
+  }
 };
 
-
-
 export const login = (userData) => async (dispatch) => {
-    try {
-        const res = await axios.post('/api/login', userData);
-        dispatch({
-            type: LOGIN_SUCCESS,
-            payload: res.data // Assuming the backend returns user data and JWT token upon successful login
-        });
-    } catch (error) {
-        dispatch({
-            type: AUTH_ERROR,
-            payload: error.response.data.message // Assuming the backend returns error message in case of failure
-        });
-    }
+  try {
+    const res = await axios.post(`${baseURL}/api/login`, userData);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data, // Assuming the backend returns user data and JWT token upon successful login
+    });
+  } catch (error) {
+    dispatch({
+      type: AUTH_ERROR,
+      payload: error.response.data.message, // Assuming the backend returns error message in case of failure
+    });
+  }
 };
 
 export const forgotPassword = (email) => async (dispatch) => {
-    try {
-        const res = await axios.post('/api/forgot-password', { email });
-        dispatch({
-            type: FORGOT_PASSWORD_SUCCESS,
-            payload: res.data // Assuming the backend returns success message upon successful password reset request
-        });
-    } catch (error) {
-        dispatch({
-            type: AUTH_ERROR,
-            payload: error.response.data.message // Assuming the backend returns error message in case of failure
-        });
-    }
+  try {
+    const res = await axios.post(`${baseURL}/api/forgot-password`, { email });
+    dispatch({
+      type: FORGOT_PASSWORD_SUCCESS,
+      payload: res.data, // Assuming the backend returns success message upon successful password reset request
+    });
+  } catch (error) {
+    dispatch({
+      type: AUTH_ERROR,
+      payload: error.response.data.message, // Assuming the backend returns error message in case of failure
+    });
+  }
 };
