950d7e0aeb1670771d6b650f3a4fff23e44cf24a
mirzahassanI <165846334+mirzahassanI@users.noreply.github.com>
2024-05-14 22:53:54 +0500

    Update Togglebutton.jsx

--- my-app/src/components/Togglebutton.jsx
+++ my-app/src/components/Togglebutton.jsx
@@ -5,10 +5,11 @@ import { logout } from "../actions/authActions"; // Adjust the path as needed
 import { Alert, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
-const LanguageToggleButton = ({ onLanguageChange }) => {
+const LanguageToggleButton = ({ isSetLanguageButton, onLanguageChange }) => {
   const { i18n } = useTranslation();
   const [showAlert, setShowAlert] = useState(false);
   const navigate = useNavigate();
+  const currentLanguage = i18n.language;
 
   const dispatch = useDispatch();
 
@@ -28,18 +29,40 @@ const LanguageToggleButton = ({ onLanguageChange }) => {
 
   return (
     <div>
-      {/* <Button onClick={() => changeLanguage("en")}>English</Button>
-      <Button onClick={() => changeLanguage("ar")}>العربية</Button> */}
-      <div className="fixed-bottom ml-md-5">
-       
-          {showAlert && (
-            <Alert variant="danger" className="w-25">
-              Logout successful.
-            </Alert>
-          )}
-          <Button onClick={handleLogout} variant="danger" className="ml-md-25">Logout</Button>
+      {isSetLanguageButton && (
+        <div className="flex">
+          <button
+            className={`${
+              currentLanguage === "en"
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-700"
+            } px-4 py-2 rounded-l-md`}
+            onClick={() => changeLanguage("en")}
+          >
+            English
+          </button>
+          <button
+            className={`${
+              currentLanguage === "ar"
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-700"
+            } px-4 py-2 rounded-r-md`}
+            onClick={() => changeLanguage("ar")}
+          >
+            Arabic
+          </button>
         </div>
-      
+      )}
+      <div className="fixed-bottom ml-md-5">
+        {showAlert && (
+          <Alert variant="danger" className="w-25">
+            Logout successful.
+          </Alert>
+        )}
+        <Button onClick={handleLogout} variant="danger" className="ml-md-25">
+          Logout
+        </Button>
+      </div>
     </div>
   );
 };
