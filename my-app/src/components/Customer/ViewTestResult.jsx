d10902ae69b7b5338384fc22508018e597f5cf55
mirzahassanI <165846334+mirzahassanI@users.noreply.github.com>
2024-06-05 20:19:55 +0500

    Update ViewTestResult.jsx

--- my-app/src/components/Customer/ViewTestResult.jsx
+++ my-app/src/components/Customer/ViewTestResult.jsx
@@ -11,98 +11,7 @@ const ViewTestResult = () => {
   const { results, error } = useSelector((state) => state.results);
   const [searchTerm, setSearchTerm] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalContent, setModalContent] = useState([]);
-
-  // const mockResult2 = [
-  //   {
-  //     id: 1,
-  //     candidate_name: "candidate1",
-  //     candidate_email: "candidate1@gmail.com",
-  //     assessments: [
-  //       {
-  //         name: "assessment1",
-  //         tests: [
-  //           {
-  //             name: "test1",
-  //             total_questions: 1,
-  //             attempted_questions: 0,
-  //             status: "Not attempted",
-  //           },
-  //         ],
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     id: 2,
-  //     candidate_name: "candidate2",
-  //     candidate_email: "candidate2@gmail.com",
-  //     assessments: [
-  //       {
-  //         name: "assessment2",
-  //         tests: [
-  //           {
-  //             name: "testa",
-  //             total_questions: 1,
-  //             attempted_questions: 1,
-  //             score: 60,
-  //             status: "Attempted",
-  //           },
-  //           {
-  //             name: "testb",
-  //             total_questions: 1,
-  //             attempted_questions: 1,
-  //             score: 60,
-  //             status: "Attempted",
-  //           },
-  //           {
-  //             name: "testc",
-  //             total_questions: 1,
-  //             attempted_questions: 1,
-  //             score: 60,
-  //             status: "Attempted",
-  //           },
-  //         ],
-  //         assessment_score: 60,
-  //         assessment_percentage: 60,
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     id: 3,
-  //     candidate_name: "mirza",
-  //     candidate_email: "user1@gmail.com",
-  //     assessments: [
-  //       {
-  //         name: "assessment3",
-  //         tests: [
-  //           {
-  //             name: "test-alpha",
-  //             total_questions: 1,
-  //             attempted_questions: 1,
-  //             score: 80,
-  //             status: "Attempted",
-  //           },
-  //           {
-  //             name: "test-beta",
-  //             total_questions: 1,
-  //             attempted_questions: 1,
-  //             score: 20,
-  //             status: "Attempted",
-  //           },
-  //           {
-  //             name: "test-btavo",
-  //             total_questions: 1,
-  //             attempted_questions: 1,
-  //             score: 20,
-  //             status: "Attempted",
-  //           },
-  //         ],
-  //         assessment_score: 40,
-  //         assessment_percentage: 40,
-  //       },
-  //     ],
-  //   },
-  // ];
+  const [selectedTests, setSelectedTests] = useState([]);
 
   useEffect(() => {
     dispatch(getUserResults()); // Dispatch the fetchResults action when component mounts
@@ -111,6 +20,17 @@ const ViewTestResult = () => {
   const filteredResults = results.filter((candidate) =>
     candidate.candidate_name.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  // Collect all unique test names
+  const testNames = [
+    ...new Set(
+      results.flatMap((candidate) =>
+        candidate.assessments.flatMap((assessment) =>
+          assessment.tests.map((test) => test.name)
+        )
+      )
+    ),
+  ];
 
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10;
@@ -124,13 +44,33 @@ const ViewTestResult = () => {
 
   const handlePageChange = (page) => setCurrentPage(page);
 
-  const handleViewMore = (tests) => {
-    setModalContent(tests.slice(2));
-    setIsModalOpen(true);
+  const getStatusMessage = (status) => {
+    if (status.includes("Not attempted any questions from this test")) {
+      return "Not Attempted";
+    } else if (status.includes("Attempted")) {
+      const attemptedCount = status.match(/Attempted (\d+) questions/);
+      if (attemptedCount && attemptedCount[1]) {
+        return "Incomplete";
+      }
+    }
+    return status;
+  };
+
+  const openModal = (tests) => {
+    if (tests.length > 2) {
+      const filteredTests = tests.slice(2); // Filter out the first two tests
+      setSelectedTests(filteredTests);
+      setIsModalOpen(true);
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedTests([]);
   };
 
   return (
-    <div className=" rounded-xl p-6 min-h-screen font-roboto">
+    <div className="rounded-xl p-6 min-h-screen font-roboto">
       <div className="flex items-center mb-4">
         <TiChartBarOutline className="mr-2" size={24} />
         <h1 className="text-3xl font-bold mt-1 text-gray-700 font-roboto">
@@ -150,18 +90,31 @@ const ViewTestResult = () => {
       <table className="min-w-full divide-y divide-gray-200">
         <thead className="bg-gray-50">
           <tr>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{t("candidatesResult.Name")}</th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              {t("candidatesResult.Name")}
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
               {t("candidatesResult.assessmentName")}
             </th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              {t("candidatesResult.testName")}
-            </th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{t("candidatesResult.Status")}</th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              {t("candidatesResult.testScore")}
-            </th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            {testNames.slice(0, 2).map((testName, index) => (
+              <th
+                key={index}
+                scope="col"
+                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                {testName}
+              </th>
+            ))}
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
               {t("candidatesResult.assessmentScore")}
             </th>
           </tr>
@@ -182,83 +135,47 @@ const ViewTestResult = () => {
                 <tr
                   key={`${candidate.id}-${index}`}
                   className="hover:bg-active-link-bg cursor-pointer transition duration-150"
-                  >
+                >
                   <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                     {candidate.candidate_name}
                   </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{assessment.name}</td>
-
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500" style={{ width: "300px" }}>
-                    {assessment.tests.slice(0, 2).map((test, index2) => (
-                      <div key={index2}>
-                        {test.name}
-                        {index2 === 1 && assessment.tests.length > 2 && (
-                          <span
-                            className="text-blue-500 cursor-pointer"
-                            onClick={() => handleViewMore(assessment.tests)}
-                          >
-                            , view more
-                          </span>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {assessment.name}
+                  </td>
+                  {testNames.slice(0, 2).map((testName, index2) => {
+                    const test = assessment.tests.find(
+                      (test) => test.name === testName
+                    );
+                    return (
+                      <td
+                        key={index2}
+                        className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
+                        onClick={() => openModal(assessment.tests)} // Add onClick event
+                      >
+                        {test ? (
+                          <>
+                            {test.score ? `${test.score}%` : "0%"}
+                            <span
+                              className="px-2 py-1 ml-2 rounded"
+                              style={{ backgroundColor: "#ccffcc" }}
+                            >
+                              {getStatusMessage(test.status)}
+                            </span>
+                          </>
+                        ) : (
+                          "-"
                         )}
-                      </div>
-                    ))}
-                  </td>
-
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    <span>
-                      {assessment.tests.slice(0, 2).map((test, index2) => (
-                        <React.Fragment key={index2}>
-                          {test.status.includes("Attempted 1 questions") ||
-                          test.status.includes("Attempted 2 questions")
-                            ? "Incomplete"
-                            : test.status.includes(
-                                "Not attempted any questions from this test"
-                              )
-                            ? "Not Attempted"
-                            : test.status}
-                          {index2 === 0 && assessment.tests.length > 1 && ", "}
-                        </React.Fragment>
-                      ))}
-                    </span>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    <span>
-                      {assessment.tests.slice(0, 2).map((test, index2) => (
-                        <React.Fragment key={index2}>
-                          {test.score !== null
-                            ? test.score
-                              ? `${test.score}%`
-                              : "0"
-                            : "-"}
-                          {index2 === 0 && assessment.tests.length > 1 && ", "}
-                        </React.Fragment>
-                      ))}
-                    </span>
-                  </td>
+                      </td>
+                    );
+                  })}
                   <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                     <span>
                       {assessment.assessment_percentage !== null
                         ? assessment.assessment_percentage !== 0
                           ? `${assessment.assessment_percentage}%`
-                          : "0"
+                          : "0%"
                         : "-"}
                     </span>
-
-                    {/* {assessment.tests.slice(0, 2).map((test, index2) => (
-                      <span
-                        key={index2}
-                        className={`px-2 py-1 rounded-full ${
-                          test.score
-                            ? test.score >= 50
-                              ? "bg-green-500"
-                              : "bg-red-500"
-                            : ""
-                        }`}
-                      >
-                        {index2 > 0 ? ", " : ""}
-                        {test.score ? `${test.score}%` : "-"}
-                      </span>
-                    ))} */}
                   </td>
                 </tr>
               ))
@@ -272,44 +189,38 @@ const ViewTestResult = () => {
         onPageChange={handlePageChange}
       />
       {isModalOpen && (
-        <Modal tests={modalContent} onClose={() => setIsModalOpen(false)} />
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+          <div className="bg-white rounded-lg shadow-lg p-4 w-2/3">
+            <h2 className="text-xl font-bold mb-4">Tests</h2>
+            <table className="w-full table-auto border-collapse">
+              <thead>
+                <tr>
+                  <th className="border px-4 py-2">Test Name</th>
+                  <th className="border px-4 py-2">Status</th>
+                  <th className="border px-4 py-2">Test Score</th>
+                </tr>
+              </thead>
+              <tbody>
+                {selectedTests.map((test, index) => (
+                  <tr key={index} className="border-b">
+                    <td className="border px-4 py-2">{test.name}</td>
+                    <td className="border px-4 py-2">{test.status}</td>
+                    <td className="border px-4 py-2">
+                      {test.score ? `${test.score}%` : "-"}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <button
+              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+              onClick={closeModal}
+            >
+              Close
+            </button>
+          </div>
+        </div>
       )}
-    </div>
-  );
-};
-
-const Modal = ({ tests, onClose }) => {
-  return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-      <div className="bg-white rounded-lg shadow-lg p-4 w-2/3">
-        <h2 className="text-xl font-bold mb-4">Tests</h2>
-        <table className="w-full table-auto border-collapse">
-          <thead>
-            <tr>
-              <th className="border px-4 py-2">Test Name</th>
-              <th className="border px-4 py-2">Status</th>
-              <th className="border px-4 py-2">Test Score</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tests.map((test, index) => (
-              <tr key={index} className="border-b">
-                <td className="border px-4 py-2">{test.name}</td>
-                <td className="border px-4 py-2">{test.status}</td>
-                <td className="border px-4 py-2">
-                  {test.score ? `${test.score}%` : "-"}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <button
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-          onClick={onClose}
-        >
-          Close
-        </button>
-      </div>
     </div>
   );
 };
