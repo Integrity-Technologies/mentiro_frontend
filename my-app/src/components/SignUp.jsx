088a06261c6d3d1bb6d3a321b13c54b83d57ca13
Hamzaintegritydev <hamza.naeem@integrity.pk>
2024-07-05 06:24:57 +0500

    implement figma design dashbaord and create assesment page

--- my-app/src/components/SignUp.jsx
+++ my-app/src/components/SignUp.jsx
@@ -575,15 +575,21 @@ const SignUp = () => {
                         placeholder={t("signup.select_job_title")}
                       />
                       {formData.jobTitle === "Other" && (
-                        <div className="mb-3 mt-3">
+                        <div className="mb-3 mt-3 relative">
                           <input
                             type="text"
                             name="jobTitleCustom"
-                            value={jobTitleCustom} // Ensure this is linked to the jobTitleCustom state
-                            onChange={handleChange} // and uses handleChange
-                            className="block px-2.5 pb-2 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                            placeholder="Enter your job title"
+                            value={jobTitleCustom}
+                            onChange={handleChange}
+                            className="block px-2.5 pb-2 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                            placeholder=""
                           />
+                          <label
+                            htmlFor="jobTitleCustom"
+                            className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                          >
+                            Custom Job Title
+                          </label>
                         </div>
                       )}
                       {errors.jobTitle && (
@@ -591,8 +597,8 @@ const SignUp = () => {
                           {errors.jobTitle}
                         </span>
                       )}
-                    </div>
-                  </div>
+                      </div>
+                      </div>
                   <div className="flex items-start mb-3">
                     <div className="flex items-center h-5">
                       <input
